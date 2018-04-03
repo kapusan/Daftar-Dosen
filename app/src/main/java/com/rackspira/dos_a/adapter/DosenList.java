31a64391f86e2d1a79071f674aafcaf34c93c83d
Anang S <anang120799@gmai.com>
2018-04-03 20:54:47 +0700

    Layouting

---
+++ app/src/main/java/com/rackspira/dos_a/adapter/DosenList.java
@@ -0,0 +1,8 @@
+package com.rackspira.dos_a.adapter;
+
+/**
+ * Created by Anang S on 03/04/2018.
+ */
+
+public class DosenList {
+}
--- app/src/main/java/com/rackspira/dos_a/view/MainActivity.java
+++ app/src/main/java/com/rackspira/dos_a/view/MainActivity.java
@@ -2,14 +2,49 @@ package com.rackspira.dos_a.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.rackspira.dos_a.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinnerMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerMain = (Spinner) findViewById(R.id.spinner_main);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.
+                createFromResource(this, R.array.pilihan, R.layout.support_simple_spinner_dropdown_item);
+        spinnerMain.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_credit:
+
+                break;
+            case R.id.menu_about:
+
+                break;
+            case R.id.menu_exit:
+                finish();
+                break;
+        }
+        return true;
     }
 }
