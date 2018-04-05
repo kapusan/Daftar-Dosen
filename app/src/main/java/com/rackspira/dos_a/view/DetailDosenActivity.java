d68983aeff6c5294fb1c012592f43e317d8e91f4
Anang S <anang120799@gmai.com>
2018-04-05 20:28:01 +0700

    add DetailDosenActivity tapi masih kosong

---
+++ app/src/main/java/com/rackspira/dos_a/view/DetailDosenActivity.java
@@ -0,0 +1,15 @@
+package com.rackspira.dos_a.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rackspira.dos_a.R;
+
+public class DetailDosenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_dosen);
+    }
+}
